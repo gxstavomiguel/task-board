fb75ca70c0c7e21870910782ea25627dcd33ea21
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2025-05-28 16:15:58 -0300

    upt

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (): Promise<unknown> =>
-  bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
